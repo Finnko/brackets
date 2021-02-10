b186331eafe3e1328a33175c54fb928a0edd9f7e
Alexey Khabirov <Khabirov.A@citilink.ru>
2021-02-10 18:32:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    const bracketsMap = new Map();
+
+    for (const [open, close] of bracketsConfig) {
+        bracketsMap.set(open, close);
+    }
+
+    for (const char of str) {
+        if (stack.length !== 0) {
+            if (char === bracketsMap.get(stack[stack.length - 1])) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return !stack.length;
 }
